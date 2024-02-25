17f4e32e6262bdb2a716d20cadecc602509d0a65
KingMonkey <japzgang@gmail.com>
2024-02-25 15:54:40 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using Photon.Voice;
+using ThatGTFrame;
+using Unity.Mathematics;
+using UnityEngine;
+using Utilla;
+
+namespace ThatGTFrame
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+
+        public bool active;
+        bool inRoom;
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            AssetObj.SetActive(true);
+
+            active = true;
+
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            AssetObj.SetActive(false);
+
+            active = false;
+
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+        public GameObject AssetObj;
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            var assetBundle = LoadAssetBundle("ThatGTFrame.thatgt");
+            GameObject Obj = assetBundle.LoadAsset<GameObject>("ThatGtFrame");
+
+            AssetObj = Instantiate(Obj);
+            AssetObj.transform.position = new Vector3(-68.8294F, 12.382F, -84.1987F);
+            AssetObj.transform.rotation = Quaternion.Euler(270f, 151.0634f, 0f);
+            AssetObj.transform.localScale = new Vector3(3.427f, 47.7546f, 53.7565f);
+        }
+
+        AssetBundle LoadAssetBundle(string path)
+        {
+            try
+            {
+                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+                AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+                stream.Close();
+                Debug.Log("[" + PluginInfo.GUID + "] Success loading asset bundle");
+                return bundle;
+            }
+            catch (Exception e)
+            {
+                Debug.Log("[" + PluginInfo.Name + "] Error loading asset bundle: " + e.Message + " " + path);
+                throw;
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ThatGTFrame
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.kingmonkey.gorillatag.thatgtframe";
+		public const string Name = "ThatGTFrame";
+		public const string Version = "1.0.0";
+	}
+}
